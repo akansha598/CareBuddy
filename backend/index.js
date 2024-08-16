8f33024a4ffc4be51cb0c2979f51c062881929dd
Samarth Narayan <kayaniv321@gmail.com>
2024-08-17 01:11:40 +0530

    backend and database setup completed

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+        .then(()=>{ console.log("Database connected!!"); })
+        .catch((err)=>{ console.log(err) }); 
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000!!');
+});
+
+app.get("/test",(req,res,next)=>{
+    return res.send("Hello");
+});
