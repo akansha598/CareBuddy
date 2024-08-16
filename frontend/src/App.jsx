cd64f6d51e2a8dbc962e6adcebed569e477a3168
Samarth Narayan <kayaniv321@gmail.com>
2024-08-17 01:34:16 +0530

    fronten files added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import SignUp from './pages/SignUp.jsx';
+import SignIn from './pages/SignIn.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
 export default function App() {
   return (
-    <h1 className="font-extrabold text-red-100 underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />          
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
