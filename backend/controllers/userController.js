a737b4450b4b7c311facd25e8649b874064dbfae
Samarth Narayan <kayaniv321@gmail.com>
2024-08-18 00:01:49 +0530

    signin controller created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../Models/userModel.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const getAllUsers = async(req,res,next) =>{}
 
@@ -24,5 +25,35 @@ export const signup = async(req,res,next) =>{
     catch(err){
         res.status(402)
            .json('User already exists!');
+    }
+}
+
+export const signin = async(req,res,next) =>{
+    const {email,username,password}=req.body;
+    if(!username || !email || !password || username==='' || password==='' || email==='')
+    res.status(499)
+        .json('All fields are required!');
+    try{
+        const validUser=await User.findOne({email});
+        //console.log(validUser);
+        if(!validUser)
+        return res.status(404)
+                  .json('User not found!');
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword)
+        return res.status(401)
+                  .json('Wrong credentials!');        
+        const token=jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        //console.log(token);
+        const {password:pass, ...rest}=validUser._doc;
+        res.status(200)
+           .cookie('access_token',token,{
+            httpOnly:true,
+           })
+           .json(rest);
+    }
+    catch(err){
+        res.status(401)
+           .json('Signin Unsuccessfull!');
     }
 }
