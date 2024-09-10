8fc76b569121fa0b142d9e347b2f31bd74f135d0
rajankita098 <rajankita098@gmail.com>
2024-09-10 12:36:37 +0530

    updated main

--- frontend/backend/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {getAllAdmins, signupAdmin} from '../controllers/adminController.js';
-
-const router=express.Router();
-
-router.get("/",getAllAdmins);
-router.post("/signup",signupAdmin);
-
-export default router;  
